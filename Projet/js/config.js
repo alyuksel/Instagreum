09fb0844dcc7694500ec65781337b32cce62fec9
Adam <adam.mihoubi@etu.u-pec.fr>
2017-12-07 10:11:08 +0100

    corr

--- Projet/js/config.js
+++ Projet/js/config.js
@@ -5,5 +5,5 @@ app_ex15.config(function($routeProvider){
   .when('/test',{templateUrl:'views/test.html',controller:''})
   .when('',{templateUrl:'views/main.html',controller:''})
   .otherwise({redirectTo:''});
-  console.log("config.js");
+
 });
