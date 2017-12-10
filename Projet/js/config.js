2748b07fb8ede97ba1dde2193f831758968b6498
adam.mihoubi <adam.mihoubi@etu.u-pec.fr>
2017-12-10 17:30:36 +0100

    ajout des controlleur et debut de la gestion des auth

--- Projet/js/config.js
+++ Projet/js/config.js
@@ -1,9 +1,25 @@
 var app=angular.module('myApp',['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
-  .when('/home',{templateUrl:'views/home.html',controller:''})
-  .when('/test',{templateUrl:'views/test.html',controller:''})
+  .when('/home',{templateUrl:'views/home.html',controller:'homeController'})
+  .when('/test',{templateUrl:'views/test.html',controller:'testController'})
+  .when('/pres',{templateUrl:'views/pres.html',controller:'presController'})
   .when('',{templateUrl:'views/main.html',controller:''})
   .otherwise({redirectTo:''});
 
 });
+app.controller('homeController', function($scope) {
+
+    $scope.text = "hello world";
+});
+app.controller('testController', function($scope) {
+
+    $scope.text = "hello world";
+});
+
+//TODO: trouver un moyen de crypter le mot de passe entré avant de l'envoyer à l'api
+app.controller('presController',['$scope', function($scope) {
+     $scope.password = sha256('');
+     $scope.username = '';
+
+}]);
