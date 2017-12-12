9e8ecb8e589563e8058c60de31f5d90fc6e2703c
alpi <alpi94120@gmail.com>
2017-12-12 18:49:22 +0100

    fix merge bug

--- Projet/js/controllers/createUser.js
+++ Projet/js/controllers/createUser.js
@@ -1,35 +1,4 @@
-<<<<<<< HEAD
 angular.module('createUser-controller',[]).controller('createUserController', function($scope,$location,$http) {
-  $scope.username ="";
-  $scope.name ="";
-  $scope.firstname="";
-  $scope.password="";
-  $scope.password2="";
-  $scope.birthdate="";
-  $scope.mail="";
-  $scope.error="";
-  $scope.valid = function()
-  {
-    if($scope.password != $scope.password2){
-      $scope.error="vos mots de passes sont differents";
-    }else{
-      var data = JSON.stringify({
-        username:$scope.username,
-        name:$scope.name,
-        firstname:$scope.firstname,
-        birthdate:$scope.birthdate,
-        password:sha256($scope.password),
-        mail:$scope.mail
-      });
-      var username = $scope.username;
-      var get = $http.get('/api/login/'+username, data);
-      get.then(function(response){
-        
-        var post = $http.post('/api/createUser', data);
-        post.then(function(response){
-           $location.path('/login');
-=======
-  angular.module('createUser-controller',[]).controller('createUserController', function($scope,$location,$http) {
     $scope.username ="";
     $scope.name ="";
     $scope.firstname="";
@@ -94,10 +63,7 @@ angular.module('createUser-controller',[]).controller('createUserController', fu
           },function(response){
               $scope.error="erreur dans la saisies des informations";
           });
->>>>>>> structure
-        },function(response){
-            $scope.error="erreur dans la saisies des informations";
-        });
 
     }
-  });
+  })};
+}
