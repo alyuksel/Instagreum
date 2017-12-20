1a09920c5f87e215fd3013c23886456ca1459ade
adam.mihoubi <adam.mihoubi@etu.u-pec.fr>
2017-12-20 16:18:38 +0100

    getImage on screen

--- Projet/js/controllers/profile.js
+++ Projet/js/controllers/profile.js
@@ -1,6 +1,26 @@
-angular.module('profile-controller',[]).controller('profileController', function($location,$cookies,$scope) {
+angular.module('profile-controller',[]).controller('profileController', function($location,$cookies,$scope,$sce,$http) {
     if($cookies.get("current") == null){
       $location.path("/login");
     }
-    $scope.text = $cookies.get("current");
+
+    $scope.mimetype='';
+
+    var currentUser = $cookies.get("current");
+    $scope.user=currentUser;
+    var get = $http.get("/api/getImage/"+currentUser);
+    get.then(function(res)
+    {
+      console.log(res);
+      $scope.mimetype=  res.data.img.contentType;
+      $scope.data = res.data.img.data.data;
+      // var file = new Blob([btoa(res.data.img.data.data)],{type : res.data.img.contentType});
+      // console.log(file);
+      // url = URL.createObjectURL(file);
+      // $scope.data = $sce.trustAsResourceUrl(url);
+
+    },
+    function(res){
+      console.log(res);
+    });
+
 });
