59b911844be0b5f849349b30fe6059845c1ad13c
alpi <alparslan.yuksel@etu.u-pec.fr>
2017-12-26 19:44:30 +0100

    add reloading after post photo

--- Projet/js/controllers/profile.js
+++ Projet/js/controllers/profile.js
@@ -1,31 +1,33 @@
 angular.module('profile-controller',[]).controller('profileController', function($location,$cookies,$scope,$sce,userService,imageService) {
-  if($cookies.get("current") == null){
-    $location.path("/login");
-  }else{
-
-    var currentUser = $cookies.get("current");
-    $scope.user=currentUser;
+  $scope.getProfileImages = function(){
     imageService.getImagesByUser(currentUser,function(res){
       $scope.images = res.data.map(function (item){
          return {mimetype:item.img.contentType,data:_arrayBufferToBase64(item.img.data.data)};
       });
-
     },
     function(res){
       console.log("BOOOO");
     });
-    $scope.uploadFile = function(){
-      var file = new FormData();
-      console.log($scope.img);
-      file.append('img',$scope.img);
-      console.log(file);
-      imageService.sendImageByUser($scope.user,file,function(res){
-        console.log("succes");
-      },
-      function(){
-        console.log("echec");
-      });
-    };
-
   }
+  if($cookies.get("current") == null){
+    $location.path("/login");
+  }else{
+    var currentUser = $cookies.get("current");
+    $scope.user=currentUser;
+    $scope.getProfileImages();
+  }
+  $scope.uploadFile = function(){
+    var file = new FormData();
+    console.log($scope.img);
+    file.append('img',$scope.img);
+    console.log(file);
+    imageService.sendImageByUser($scope.user,file,function(res){
+      $scope.getProfileImages();
+      console.log("succes");
+    },
+    function(){
+      console.log("echec");
+    });
+  };
+
 });
