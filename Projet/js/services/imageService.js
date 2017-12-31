32521c3a16597dcd2e2d91570861e5189dc5b2f1
adam.mihoubi <adam.mihoubi@etu.u-pec.fr>
2017-12-31 21:15:03 +0100

    Merge branch 'acceuil' into develop

--- Projet/js/services/imageService.js
+++ Projet/js/services/imageService.js
@@ -15,11 +15,25 @@ angular.module("image-service",[]).factory('imageService', function($http){
 				$http.get("/api/images").then(success,error);
 	};
 
+
 	user.updateComment = function(user,image,success,error){
 		console.log(image);
 		var data = new FormData();
 		data.append('comment',image.commentaire);
 		$http.put("/api/images/comment/"+image.id,{"comment":image.commentaire}).then(success,error);
+	};
+
+	user.isLiked = function(username,success,error){
+				$http.get("api/images/like/"+username).then(success,error);
+	};
+
+	user.likeImage = function(username,id,success,error,successInc){
+			$http.post("/api/images/like/"+username+"/"+id).then(success,error);
+			$http.post("/api/images/addLike/"+id).then(successInc);
+	};
+
+	user.getComments = function(success,error){
+			$http.get("api/images/comments").then(success,error);
 	};
 	return user;
 });
